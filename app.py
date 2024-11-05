c96a0b4c11a7b2d4045c7a6bf4ce33945f259107
JARP <andersonandy046@gmail.com>
2024-11-05 10:55:53 +0800

    https://github.com/anderson895/ProjectTeach

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def landingls():
+def landing():
     message = "Hello, this message is dynamic!"
     return render_template('index.html', message=message)
 
 @app.route('/admin/login')
 def admin_login():
     return render_template('admin/login.html')
+
+@app.route('/admin/register')
+def admin_register():
+    return render_template('admin/registration.html')
 
 if __name__ == '__main__':
     app.run(port=5001)
